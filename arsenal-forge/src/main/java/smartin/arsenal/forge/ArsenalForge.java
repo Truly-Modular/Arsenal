6c53aac4ba5ec65be59ef0ec83381ce1a133cf3c
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-03 06:09:37 +0100

    - forge fixes, versioning, and moduleIconProperty

--- arsenal-forge/src/main/java/smartin/arsenal/forge/ArsenalForge.java
+++ arsenal-forge/src/main/java/smartin/arsenal/forge/ArsenalForge.java
@@ -1,19 +1,10 @@
 package smartin.arsenal.forge;
 
-import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.fml.common.Mod;
 import smartin.arsenal.Arsenal;
 
 @Mod(Arsenal.MOD_ID)
 public class ArsenalForge {
     public ArsenalForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(Arsenal.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-            Arsenal.init();
-        Event event;
-        AttackEntityEvent event1;
     }
 }
