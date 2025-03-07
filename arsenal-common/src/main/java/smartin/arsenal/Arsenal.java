1ecf5872ba82b93fe5f5470fa9af8208e7d78b5e
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-07 18:21:00 +0100

    - fixed kubejs, adjusted armor hardness math

--- arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
+++ arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
@@ -1,10 +1,8 @@
 package smartin.arsenal;
 
-import dev.architectury.event.events.common.LifecycleEvent;
-
 public class Arsenal
 {
-	public static final String MOD_ID = "arsenal";
+	public static final String MOD_ID = "tm_arsenal";
 
 	public static void init() {
 		//LifecycleEvent.SETUP.register(GenerateModularConverters::new);
