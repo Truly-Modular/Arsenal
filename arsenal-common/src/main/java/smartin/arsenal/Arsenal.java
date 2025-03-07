609775faeabfbbb3f84c969ad5c8a5ef1e289e50
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-07 23:05:37 +0100

    - fixed neo

--- arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
+++ arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
@@ -1,10 +1,15 @@
 package smartin.arsenal;
 
-public class Arsenal
-{
-	public static final String MOD_ID = "tm_arsenal";
+import dev.architectury.event.EventResult;
+import smartin.miapi.events.MiapiEvents;
 
-	public static void init() {
-		//LifecycleEvent.SETUP.register(GenerateModularConverters::new);
-	}
+public class Arsenal {
+    public static final String MOD_ID = "tm_arsenal";
+
+    public static void init() {
+        MiapiEvents.GENERATE_MATERIAL_CONVERTERS.register((material, tools, armorItems, isClient) -> {
+            GenerateConvertersHelper.setupTools(tools, material);
+            return EventResult.pass();
+        });
+    }
 }
